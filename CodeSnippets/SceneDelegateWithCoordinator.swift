3ce7b8d4c250d63b158c83a12d4cb0c5f6423f0c
Thiago Batista <66257468+tbdbatista@users.noreply.github.com>
2023-02-26 10:58:09 -0300

    Create SceneDelegateWithCoordinator.swift

---
+++ CodeSnippets/SceneDelegateWithCoordinator.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var appCoordinator: AppCoordinator?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let appCoordinator = AppCoordinator(window: window)
+        appCoordinator.start()
+        self.appCoordinator = appCoordinator
+        window.makeKeyAndVisible()
+    }
+}
